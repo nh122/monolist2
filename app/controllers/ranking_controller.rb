9145fceae850bfa72f76b77b6a926c0d6409f141
naito <ws.hiroshi.naito@gmail.com>
2016-09-19 14:06:28 +0000

    ranking

---
+++ app/controllers/ranking_controller.rb
@@ -0,0 +1,13 @@
+class RankingController < ApplicationController
+  before_action :logged_in_user
+
+  def have
+    array = Have.group(:item_id).order('count_id desc').count(:id).keys
+    @ranking = Item.find(array).index_by(&:id).slice(*array).values
+  end
+
+  def want
+    array = Want.group(:item_id).order('count_id desc').count(:id).keys
+    @ranking = Item.find(array).index_by(&:id).slice(*array).values
+  end
+end
