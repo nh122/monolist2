2eceacb1687184ef9e77eb846b76d320ba24e718
naito <ws.hiroshi.naito@gmail.com>
2016-09-20 08:51:14 +0000

    limit(10)

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,12 +2,12 @@ class RankingController < ApplicationController
   before_action :logged_in_user
 
   def have
-    array = Have.group(:item_id).order('count_id desc').count(:id).keys
+    array = Have.group(:item_id).order('count_id desc').limit(10).count(:id).keys
     @ranking = Item.find(array).index_by(&:id).slice(*array).values
   end
 
   def want
-    array = Want.group(:item_id).order('count_id desc').count(:id).keys
+    array = Want.group(:item_id).order('count_id desc').limit(10).count(:id).keys
     @ranking = Item.find(array).index_by(&:id).slice(*array).values
   end
 end
